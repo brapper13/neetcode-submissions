d15da5b377d949bfc3ead429e64c85376e82e7d2
brapper13 <barunmishra98@gmail.com>
2026-08-23 10:41:06 +0100

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.go
@@ -0,0 +1,35 @@
+func isPalindrome(s string) bool {
+	// s is ascii but case insensitive
+	// need to ignore non-alphanumeric characters
+	i := 0
+	j := len(s)-1
+	for i < j {
+		for i < j && !isAlphaNumeric(s[i]) {
+			i++
+		}
+		for j > i && !isAlphaNumeric(s[j]) {
+			j--
+		}
+
+		if toLowerCase(s[i]) != toLowerCase(s[j]) {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func toLowerCase(b byte) byte {
+	if 'A' <= b && b <= 'Z' {
+		return b + 'a' - 'A'
+	}
+	return b
+}
+
+func isAlphaNumeric(b byte) bool {
+	if 'a' <= b && b <= 'z' || 'A' <= b && b <= 'Z' || '0' <= b && b <= '9' {
+		return true
+	}
+	return false
+}
