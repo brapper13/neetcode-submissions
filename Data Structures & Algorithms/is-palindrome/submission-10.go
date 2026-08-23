4bfa4bf78af7229ca37ed57c3869dc86de5a548a
brapper13 <barunmishra98@gmail.com>
2026-08-23 10:23:49 +0100

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.go
@@ -0,0 +1,24 @@
+func isPalindrome(s string) bool {
+	//sanitize the input first
+	// s is ascii but case insensitive
+	// need to remove non-alphanumeric characters
+	s = strings.ToLower(s)
+	i := 0
+	j := len(s)-1
+	for i < len(s)/2 {
+		// if front is aln
+		for !('a' <= s[i] && s[i] <= 'z' || 'A' <= s[i] && s[i] <= 'Z' || '0' <= s[i] && s[i] <= '9') && i < j {
+			i++
+		}
+		for !('a' <= s[j] && s[j] <= 'z' || 'A' <= s[j] && s[j] <= 'Z' || '0' <= s[j] && s[j] <= '9') && j > i {
+			j--
+		}
+
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
