66d62dad8b8299dd21d0b5fca33eededddec6141
brapper13 <barunmishra98@gmail.com>
2026-09-05 00:25:21 +0100

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.go
@@ -0,0 +1,23 @@
+func checkInclusion(s1 string, s2 string) bool {
+	// s1 perm meaning needs to contain all letters in s1
+
+	// change to map
+	var countS1 [26]int
+	var local [26]int
+	for _, char := range s1 {
+		countS1[char-'a']++
+	}
+	left := 0
+	for i := 0; i < len(s2); i++ {
+		local[s2[i]-'a']++
+		if i - left + 1 >= len(s1) {
+			if countS1 == local {
+				return true
+			} else {
+				local[s2[left]-'a']--
+				left++
+			}
+		}
+	}
+	return false
+}
