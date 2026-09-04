ff17fb2631f6f29cfd96aab237934bc2179983ae
brapper13 <barunmishra98@gmail.com>
2026-09-04 22:03:00 +0100

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.go
@@ -0,0 +1,17 @@
+func characterReplacement(s string, k int) int {
+	// uppercase english characters - can use arrray!
+	var count [26]int
+	maxFreq := 0
+	left := 0
+	for idx, char := range s { 
+		count[char - 'A']++
+		if count[char - 'A'] > maxFreq {
+			maxFreq = count[char - 'A']
+		} 
+		if (idx - left + 1) - maxFreq > k {
+			count[s[left] - 'A']--
+			left++
+		}
+	}
+	return len(s) - left
+}
